06f7f7e4d4080cdce3729ce82549901c1d3b8dd0
Richabu <richa.bhuwania@hashedin.com>
2020-06-25 20:43:12 +0530

    Created model for user activity

---
+++ activity/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+# Create your models here.
+from core.models import User
+
+
+class UserActivity(models.Model):
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{} {} {}".format(self.user, self.start_time, self.end_time)
+
+    class Meta:
+        db_table = "user_activity"
+        verbose_name = "User Activity"
+        verbose_name_plural = "User Activities"
