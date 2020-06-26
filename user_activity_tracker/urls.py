01804820b490245eac02f8588bf6e31b9be2dbd7
Ubuntu <ubuntu@ip-172-31-45-26.ap-south-1.compute.internal>
2020-06-26 06:00:16 +0000

    Added static url conf

--- user_activity_tracker/urls.py
+++ user_activity_tracker/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('activity/', include('activity.urls')),
     # To view rest framework default login and logout page
     # path('api-auth/', include('rest_framework.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
